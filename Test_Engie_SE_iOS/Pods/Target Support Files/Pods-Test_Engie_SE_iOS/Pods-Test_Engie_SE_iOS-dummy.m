38fb69c188d3757153fea1ee0ba2d9e5d088231c
Alexandre Sagette <sagette.alexandre@gmail.com>
2017-05-31 22:26:11 +0200

    seting up AFNetworking and NVActivityIndicatorView pods

---
+++ Test_Engie_SE_iOS/Pods/Target Support Files/Pods-Test_Engie_SE_iOS/Pods-Test_Engie_SE_iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_Engie_SE_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_Test_Engie_SE_iOS
+@end
---
+++ Test_Engie_SE_iOS/Pods/Target Support Files/Pods-Test_Engie_SE_iOSTests/Pods-Test_Engie_SE_iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_Engie_SE_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Test_Engie_SE_iOSTests
+@end
