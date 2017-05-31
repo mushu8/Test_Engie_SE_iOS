65709ecb2ba0c389e59326dbb65217b2087941b9
Alexandre Sagette <sagette.alexandre@gmail.com>
2017-05-31 21:58:19 +0200

    initial xcode project with effective build & run

---
+++ Test_Engie_SE_iOS/Test_Engie_SE_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_Engie_SE_iOS
+//
+//  Created by Alexandre Sagette on 31/05/2017.
+//  Copyright © 2017 Alexandre Sagette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_Engie_SE_iOS/Test_Engie_SE_iOSTests/Test_Engie_SE_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  Test_Engie_SE_iOSTests.m
+//  Test_Engie_SE_iOSTests
+//
+//  Created by Alexandre Sagette on 31/05/2017.
+//  Copyright © 2017 Alexandre Sagette. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_Engie_SE_iOSTests : XCTestCase
+
+@end
+
+@implementation Test_Engie_SE_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
