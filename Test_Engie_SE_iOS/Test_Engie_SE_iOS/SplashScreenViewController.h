6e5e165a3044d94955836727f577fccecb706b36
Alexandre Sagette <sagette.alexandre@gmail.com>
2017-05-31 22:27:29 +0200

    renaming initial view controller

---
+++ Test_Engie_SE_iOS/Test_Engie_SE_iOS/SplashScreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  SplashScreenViewController.h
+//  Test_Engie_SE_iOS
+//
+//  Created by Alexandre Sagette on 31/05/2017.
+//  Copyright © 2017 Alexandre Sagette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+
+@end
+
